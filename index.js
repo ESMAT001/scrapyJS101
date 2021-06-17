a388a012047cc3ad2a40b940d437ffd0e4501875
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-17 12:04:52 +0430

    scrapyJS bug fixed and itz working fine

--- index.js
+++ index.js
@@ -1,25 +1,99 @@
-const scrapyJS= require('./src/scrapy')
+const scrapyJS = require('./src/scrapy')
 
 const baseURL = 'https://www.film2movie.asia/category/download-film'
 const firstPage = 1
-const lastPage = 1395
+const lastPage = 2
 
-const spider = scrapyJS(baseURL,firstPage,lastPage,{
-    nameSelector:'div.content > div > p',
-    downloadLinkSelector:"div.content > *",
-    mainPageLinkSelector:'div.title > h2 > a'
+const spider = scrapyJS(baseURL, firstPage, lastPage, {
+    nameSelector: 'div.content > div > p',
+    downloadLinkSelector: "div.content > *",
+    mainPageLinkSelector: 'div.title > h2 > a'
 })
 
-spider.on('finished',()=>{
+spider.on('finished', () => {
     console.log('crawling finished')
 })
 
-spider.on("error",(error)=>{
+spider.on("error", (error) => {
     console.log(error)
 })
 
-spider.on('crawled',(data)=>{
+spider.on('crawled', (data) => {
     console.log(data)
 })
 
+spider.override("extractDownloadLinks", (nodes) => {
+    function recursiveDlLinkExractor(el) {
+
+        for (let index = 0; index < el.children.length; index++) {
+            if (el.children[index].nodeName === "A") {
+                return el.children[index].href
+            } else {
+                const link = recursiveDlLinkExractor(el.children[index])
+                if (link) return link;
+            }
+        }
+
+        return;
+    }
+
+    function extractLinks(chunk) {
+        const downloadIdentifier = /با کیفیت/
+        const englishLangRegx = /[a-zA-Z 0-9]/g
+        const dlLinks = []
+        for (let i = 0; i < chunk.length; i++) {
+            if (downloadIdentifier.test(chunk[i].textContent)) {
+                const quality = chunk[i].textContent.match(englishLangRegx).join("").trim()
+                dlLinks.push({
+                    quality,
+                    downloadLinks: recursiveDlLinkExractor(chunk[i + 1])
+                })
+            }
+        }
+        return dlLinks;
+    }
+
+
+    let startIndex = null
+    let endIndex = null
+    for (let index = 0; index < nodes.length; index++) {
+        if (nodes[index].nodeName === "H3" && startIndex === null) {
+            startIndex = ++index
+        } else if (nodes[index].nodeName === "HR" && endIndex === null) {
+            endIndex = index
+        }
+    }
+
+    let links = Array.from(nodes).slice(startIndex, endIndex)
+    const indexes = []
+    const persianSubtitle = /زیرنویس چسبیده فارسی/
+    const dualLang = /نسخه دوبله فارسی/
+    let subLang = undefined
+    for (let index = 0; index < links.length; index++) {
+        if (links[index].nodeName === "DIV") {
+           
+            indexes.push(index)
+            if (persianSubtitle.test(links[index].textContent)) {
+                subLang = "persian_sub"
+            } else if (dualLang.test(links[index].textContent)) {
+                subLang = "dual_lang"
+            }
+        }
+    }
+
+
+    let downloadLinks = {}
+    if (!subLang) {
+        downloadLinks.original_lang = extractLinks(links)
+    } else {
+        downloadLinks = {
+            [subLang]: extractLinks(links.slice(indexes[0], indexes[1])),
+            original_lang: extractLinks(links.slice(indexes[1], indexes[2])),
+        }
+    }
+
+    return downloadLinks;
+})
+
 spider.crawl()
+// spider.crawlSinglePage("https://www.film2movie.asia/87175/%d8%af%d8%a7%d9%86%d9%84%d9%88%d8%af-%d9%81%db%8c%d9%84%d9%85-f9-2021/", 12)
--- src/scrapy.js
+++ src/scrapy.js
@@ -7,7 +7,7 @@ const { JSDOM } = jsdom;
 
 const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = {}) {
     let threads = 0
-    const maxThreads=options.maxThreads || 8
+    const maxThreads = options.maxThreads || 8
     const retryLimit = options.retryLimit || 40
     const timeOutLimit = options.timeOutLimit * 1000 || 20 * 1000
 
@@ -18,6 +18,19 @@ const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = 
             yield index++
         }
     })()
+
+    let extractDownloadLinks = function (nodes) {
+        let startIndex = null
+        let endIndex = null
+        for (let index = 0; index < nodes.length; index++) {
+            if (nodes[index].nodeName === "H3" && startIndex === null) {
+                startIndex = ++index
+            } else if (nodes[index].nodeName === "HR" && endIndex === null) {
+                endIndex = index
+            }
+        }
+        return Array.from(nodes).slice(startIndex, endIndex);
+    }
 
     const callbacks = {}
 
@@ -36,6 +49,47 @@ const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = 
         }
     }
 
+    function override(fnName, fn) {
+        switch (fnName) {
+            case 'extractDownloadLinks':
+                extractDownloadLinks = fn
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+
+
+    async function crawlSinglePage(url, page) {
+        console.log('from',page,'crawling',url)
+        try {
+
+            var html = await got(url, {
+                retry: { limit: retryLimit },
+                timeout: timeOutLimit
+            })
+            const dom = new JSDOM(html.body)
+            const movieName = dom.window.document.querySelector(options.nameSelector).textContent
+            const nodes = dom.window.document.querySelectorAll(options.downloadLinkSelector)
+
+            const downloadLinks = extractDownloadLinks(nodes)
+
+            return callbacks.onCrawled({ 'from page': page, movieName, downloadLinks })
+        } catch (error) {
+            callbacks.onError({
+                error: error,
+                url,
+                page
+            })
+            return crawlSinglePage(url, page)
+        }
+
+    }
+
+
 
     async function* scrapeMainPage(url) {
 
@@ -52,44 +106,15 @@ const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = 
         }
         const dom = new JSDOM(html.body);
         const links = dom.window.document.querySelectorAll(options.mainPageLinkSelector)
+
         console.log('main page scrapped')
+
         for (let index = 0; index < links.length; index++) {
             yield links[index].href
         }
     }
 
-    async function crawlSinglePage(url, page) {
-        try {
 
-            var html = await got(url, {
-                retry: { limit: retryLimit },
-                timeout: timeOutLimit
-            })
-            const dom = new JSDOM(html.body)
-            const movieName = dom.window.document.querySelector(options.nameSelector).textContent
-            const nodes = dom.window.document.querySelectorAll(options.downloadLinkSelector)
-            let startIndex = null
-            let endIndex = null
-            for (let index = 0; index < nodes.length; index++) {
-                if (nodes[index].nodeName === "H3" && startIndex === null) {
-                    startIndex = ++index
-                } else if (nodes[index].nodeName === "HR" && endIndex === null) {
-                    endIndex = index
-                }
-            }
-            const downloadLinks = Array.from(nodes).slice(startIndex, endIndex);
-
-            return callbacks.onCrawled({ 'from page': page, movieName, downloadLinks })
-        } catch (error) {
-            callbacks.onError({
-                error: error,
-                url,
-                page
-            })
-            return crawlSinglePage(url, page)
-        }
-
-    }
 
 
 
@@ -99,9 +124,8 @@ const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = 
         const regx = /سریال/
         const regx2 = /[a-zA-Z]/
         while (!page.done) {
-            console.log(threads)
             const url = baseURL + "/page/" + page.value + "/"
-            console.log(url)
+            console.log('threads',threads,url)
             const mainPageScrapper = scrapeMainPage(url)
             let link = await mainPageScrapper.next()
             while (!link.done) {
@@ -132,6 +156,8 @@ const scrapyJS = function (baseURL = {}, firstPage = 1, lastPage = 1, options = 
     return {
         crawl,
         on,
+        override,
+        crawlSinglePage
     }
 }
 
